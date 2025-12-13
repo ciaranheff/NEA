5f696cec02f4e6050809f4df0d49ec1b89aeeb4b
ciaranheff <ciaranhef2007@gmail.com>
2025-12-13 19:44:48 +0000

    Changed imported functions to just import

--- Main.py
+++ Main.py
@@ -1,20 +1,15 @@
 ######### Importing #############
-from Questions.Particles import Question1
-from Users.AccountManagment import AddUser
-from Users.AccountManagment import DeleteUser
-from Users.AccountManagment import PasswordChange
-
-from Users.AccountManagment import Temp
-
-from Users.AccountManagment import UserAll
-from Users.AccountManagment import UserQuestion
-from Users.AccountManagment import UserCorrect
-
 import Users.LogIn
 UL = Users.LogIn
+#################################
+import Users.AccountManagment
+UA = Users.AccountManagment
+#################################
+from Questions.Particles import Question1
 
 ######### Veriables #############
 ######### Main Loop ##########
 
+print(UA.ShowAll('Y'))
 
 print(UL.CheckPassword('Temp5',('adwi',)))
