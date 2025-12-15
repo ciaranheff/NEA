7a8ec5e847d29673610c4ff185c3b49a421a394c
ciaranheff <ciaranhef2007@gmail.com>
2025-12-15 22:11:57 +0000

    Added CheckAdmin function

--- Users/LogIn.py
+++ Users/LogIn.py
@@ -17,9 +17,18 @@ def CheckForUser (Name):
     return(found)
 
 def CheckPassword(Name,Password):
+    Password = (Password,) # SQL requests take strings as lists unless stated by a , 
     cursor.execute("SELECT Password FROM Users WHERE UserName = ?" , (Name,)) #gets password from User Database
     results = cursor.fetchall()
     if [Password] == results:
+        return True
+    else:
+        return False
+    
+def CheckAdmin(Name):
+    cursor.execute("SELECT Admin FROM Users WHERE UserName = ?" , (Name,)) #gets admin status from User Database
+    results = cursor.fetchall()
+    if [('Y',)] == results: # checks to see if admin status is true
         return True
     else:
         return False
